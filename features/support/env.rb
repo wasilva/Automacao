4fa39d72716ba5202bf6ce579aefa879bbfa28e2
William Silva <contato.wasilva@hotmail.com>
2018-07-17 02:25:27 -0300

    Feature acessar site

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 require "capybara/cucumber"
 require "selenium-webdriver"
 
-capybara.configure do |config|
+Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = "https://automacaocombatista.herokuapp.com"
   config.default_max_wait_time = 5
